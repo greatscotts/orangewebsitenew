72ca7aa5bc6bd5d3399a8cacc36317302c88e6ec
greatscotts <javelonagian@live.com>
2013-10-15 16:01:33 +0800

    email2

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,6 +1,6 @@
 <?php 
 $errors = '';
-$myemail = 'javelonagian@orangeapps.ph';//<-----Put Your email address here.
+$myemail = 'orangephils@gmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  || 
    empty($_POST['email']) || 
    empty($_POST['message']))
